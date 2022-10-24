7755cec2a56a4a55c59e3d952cd5fa2572ce6275
thinkcode <thinkcodestudio@qq.com>
2022-10-24 16:51:30 +0800

    fate: add sqlite

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -1,5 +1,10 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
+#[get("/doc")]
+async fn get_doc(file: String) -> String {
+    format!("Hello World --{file}")
+}
+
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -20,6 +25,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(echo)
+            .service(get_doc)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
